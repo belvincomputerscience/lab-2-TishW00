bb250e9fb087db4dadf838dc46d0318fa41911ea
TishW00 <94867@crowleyisdtx.me>
2024-09-02 21:57:32 -0500

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -1,1 +1,21 @@
+// Tishyra Wilson
+#include <iostream>
+int main() {
+    int num1, num2, num3, result;
+
+    std::cout << "Give me a number: ";
+    std::cin >> num1;
+    
+    std::cout << "Give me another number: ";
+    std::cin >> num2;
+    
+    std::cout << "Give me one more number: ";
+    std::cin >> num3;
+
+    result = num1 * num2 * num3;
+
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
 
